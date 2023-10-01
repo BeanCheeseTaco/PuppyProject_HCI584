a512dd992c43ffe02ee2cce785dade1588897bb3
Mary Lou <marylou@iastate.edu>
2023-10-01 15:22:32 -0600

    Removed unnecessary functions as I had some duplicates.

--- main.py
+++ main.py
@@ -7,19 +7,15 @@ import datetime
 from rapidfuzz import fuzz, process
 from PIL import Image
 import csv
-# import dutton.csv and import PetRecord.csv
-myfile = 'doc folder\dutton.csv'
-puppy_profile_file = 'doc folder\PuppyProfiles.csv'
+myfile = 'data\dutton.csv'
+puppy_profile_file = 'data\PuppyProfiles.csv'
 DOB = ''
 
-#Read file
+#Read csv file
 def load_file(csv_file=myfile):
-    with open(csv_file, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        for row in csv_reader:
-            # Process each row here
-            print(row)
-
+    df = pd.read_csv(csv_file)
+    print(df) # formatted, with leading index row
+    
 #Adds new row to file for new pet weight record
 def add_new_record(file=myfile):
     """Information is entered by the user via keyboard and stores the new data in the csv file.
@@ -70,66 +66,61 @@ def add_new_pet(file=puppy_profile_file):
         print("User data added successfully.")
     except Exception as e:
         print(f"An error occurred: {e}")
-#Plots csv file into plotly
-def plotcsv():
-    '''Plots pet csv file based on PetID selected by user. '''
-    
-# Get Pet
-def display_UI():
-    '''Print values per the unique PetIDs to generate buttons.'''
-
-# Returns all unique pets.
-def Get_All_Unique_Pets(PetData):
-    '''Loop through file and finds the uniqueID and adds to an array. Returns the array of IDs.'''
-    
-# Returns all unique pet's info per petID (petData).
-def Get_All_Info_For_Pet(PetID, PetData):
-    '''Returns array with all info pertaining to petID (Parsed specific to the unique PetID.)'''
 
 #Returns every pet's record
-def Get_All_Info_For_All_Pets(petProfile=puppy_profile_file):
-    with open(petProfile, 'r') as petProfile:
-        csv_reader = csv.reader(petProfile)
-        for row in csv_reader:
-            # Process each row here
-            print(row)
+def Get_All_Profiles(petProfile=puppy_profile_file):
+    df = pd.read_csv(petProfile)
+    print(df) # formatted, with leading index row
     '''Call Get_All_Unique_Pets() function
     Call Get_All_Info_For_Pet() function'''
 
-    
-#Prints out Pet's Records
-def print_record_for_individual(PetID):
-    '''Call Get_All_Info_For_Pet(PetID, PetData)
-    Prints pet profile based on PetID selected by user from csv.'''
-    
-#Allow updates to Profile.
-def updatedogprofile(PetID, PetProfile):
-    '''returns updated pet's profile'''
+#Prints out individual pet's entry records, e.g. an entry for a day
+def print_indvidual_entry_record(csv_file=myfile):
+    df = pd.read_csv(csv_file)
+    userSearch = input("What record do you want to return?")
+    r = df.loc[int(userSearch)]
+    print(r, "\n")
+
+    #Returns individual pet's weight records
+def print_weight_record_for_individual(csv_file=myfile):
+    df = pd.read_csv(csv_file)
+    userSearch = input("What weight do you wan to return?")
+
+    r = df["Weight"].loc[int(userSearch)]
+    #weight = df["Weight"].loc[r]
+    #print({weight})
+    print(r)
 
 # gets dog's profile based on PetID.
-def get_dogprofile(PetID, PetProfile):
+def get_uniquedogprofile(pup=puppy_profile_file):
     '''returns dog's profiles based on petID.'''
+    df = pd.read_csv(pup)
+    userSearch = input("What pet's profile do you want to see? \n1. Dutton \n2. Kiko \n 3. Copper? Press Enter.")
+
+    r = df["Weight"].loc[int(userSearch)]
+    print(r, "\n")
 
 #loads image
-def get_image():
+def get_image(pup=puppy_profile_file):
+    df = pd.read_csv(pup)
+    r = 2
+    pupImage = df["Profile Image"].loc[r]
     # load and show images
-    img = Image.open('doc folder\puppy1.jpg')
-    # image parameters
-    print(img.format, img.size, img.mode)
+    img = Image.open(pupImage)
     display(img) # in jupyter, the image is shown as output
 
-
-'''def load_file():
-    with open('doc folder\dutton.csv', 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        for row in csv_reader:
-            # Process each row here
-            print(row)
-'''
-
 #Calculates age     
-def getAge(DOB = '01/04/2023', date_of_entry = '05/02/2023'):
+def getAge(pup=puppy_profile_file, csv_file=myfile): #DOB = '01/05/2023', , date_of_entry = '05/02/2023'
     """Calculate the age based on all of the Date fields from the PetRecord.csv file and compares to the DOB field from the pets.csv file."""
+    df = pd.read_csv(pup)
+    r = 1
+    DOB = df["Date of Birth"].loc[r]
+    print({DOB})
+
+    df = pd.read_csv(csv_file)
+    r = 1
+    date_of_entry = df["DateofEntry"].loc[r]
+    print({date_of_entry})   
 
     DOButc_time = time.strptime(DOB + ' 00:00:00', "%m/%d/%Y %H:%M:%S")
     DOBepoch_time = timegm(DOButc_time)
@@ -152,9 +143,20 @@ def getAge(DOB = '01/04/2023', date_of_entry = '05/02/2023'):
     print("Months: " + str(age/(60 * 60 * 24 * 30))) # (60 seconds * 60 minutes * 24 hours * 30 days)
     print("Years: " + str(age/(3600 * 24 * 365))) # ((60 seconds * 60 minutes) = 3600 seconds * 24 hours * 365 days)
 
-get_image()
+#Plots csv file into plotly
+def plotcsv():
+    '''Plots pet csv file based on PetID selected by user. '''
+    
+# Get Pet
+def display_UI():
+    '''Print values per the unique PetIDs to generate buttons.'''
+
+#get_image()
 #add_new_record(myfile)
-load_file()
+#load_file()
 #getAge()
-Get_All_Info_For_All_Pets()
+#print_indvidual_entry_record
+#print_weight_record_for_individual()
+#Get_All_Profiles()
+#get_uniquedogprofile()
 #add_new_pet()
