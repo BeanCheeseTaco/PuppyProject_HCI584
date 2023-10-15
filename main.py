383c7d5ced8c92b8d92a8dec767231552c7c4a4f
Mary Lou <marylou@iastate.edu>
2023-10-15 13:13:50 -0600

    Creating backup to main.py.
    Going to merge pull request from professor to fix the add_new_record() function.

--- main.py
+++ main.py
@@ -2,34 +2,43 @@
 import pandas as pd
 import time
 from calendar import timegm
-from time import strftime
+#from time import strftime
 import datetime
 from rapidfuzz import fuzz, process
 from PIL import Image
 import csv
-myfile = 'data\dutton.csv'
+petFile = ['data\Dutton.csv', 'data\Max.csv', 'data\Oliver.csv']
 puppy_profile_file = 'data\PuppyProfiles.csv'
 DOB = ''
 
 #Read csv file
-def load_file(csv_file=myfile):
+def load_file(fileIndex):
+    #usersearch = input("Which profile would you like to review?")
+    csv_file=petFile[fileIndex]
     df = pd.read_csv(csv_file)
-    print(df) # formatted, with leading index row
-    
+    #print(int(usersearch))
+    #print(df) #formatted, with leading index row
+    return(df)
+
+def whichfile():
+    usersearch = input("Which profile would you like to review?")
+    return int(usersearch)
+
 #Adds new row to file for new pet weight record
-def add_new_record(file=myfile):
-    """Information is entered by the user via keyboard and stores the new data in the csv file.
-    For loop that will read through csv files and appends a new row."""
+def add_new_weight_record(fileIndex):
+
+    csv_file=petFile[fileIndex]
+
     input_date = input("Input Date: ")
     weight = input("Input weight: ")
-    comment = input("Input weight: ")
-
+    image = input("Select Image: ")
+    comment = input("Input comment: ")
     # Define the user data as a list
-    user_data = [input_date, weight, comment]
+    user_data = [input_date, weight, image, comment]
 
     try:
         # Open the CSV file in append mode
-        with open(file, mode='a', newline='') as csvfile:
+        with open(csv_file, mode='a', newline='') as csvfile:
             # Create a CSV writer
             writer = csv.writer(csvfile)
             
@@ -40,19 +49,18 @@ def add_new_record(file=myfile):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+
 #Adds a new pet to file.
 def add_new_pet(file=puppy_profile_file):
-    """Information is entered by the user via keyboard and stores the new data in the csv file.
-    For loop that will read through csv files and appends a new row. """
-    """Information is entered by the user via keyboard and stores the new data in the csv file.
-    For loop that will read through csv files and appends a new row."""
+
+    pet_ID = input("Input PetID:")
     pet_name = input("Input Pet's Name: ")
     DOB = input("Input pet's Date of Birth: ")
     breed = input("Input Breed: ")
     image = input("Input Image: " )
 
     # Define the user data as a list
-    user_data = [pet_name, DOB, breed, image]
+    user_data = [pet_ID, pet_name, DOB, breed, image]
 
     try:
         # Open the CSV file in append mode
@@ -67,22 +75,25 @@ def add_new_pet(file=puppy_profile_file):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-#Returns every pet's record
+#Returns every pet's record from petprofile csv
 def Get_All_Profiles(petProfile=puppy_profile_file):
+    
     df = pd.read_csv(petProfile)
     print(df) # formatted, with leading index row
-    '''Call Get_All_Unique_Pets() function
-    Call Get_All_Info_For_Pet() function'''
 
 #Prints out individual pet's entry records, e.g. an entry for a day
-def print_indvidual_entry_record(csv_file=myfile):
+def print_indvidual_entry_record(fileIndex):
+
+    csv_file=petFile[fileIndex]
     df = pd.read_csv(csv_file)
     userSearch = input("What record do you want to return?")
     r = df.loc[int(userSearch)]
     print(r, "\n")
 
     #Returns individual pet's weight records
-def print_weight_record_for_individual(csv_file=myfile):
+def print_weight_record_for_individual(fileIndex):
+    
+    csv_file=petFile[fileIndex]
     df = pd.read_csv(csv_file)
     userSearch = input("What weight do you wan to return?")
 
@@ -91,27 +102,10 @@ def print_weight_record_for_individual(csv_file=myfile):
     #print({weight})
     print(r)
 
-# gets dog's profile based on PetID.
-def get_uniquedogprofile(pup=puppy_profile_file):
-    '''returns dog's profiles based on petID.'''
-    df = pd.read_csv(pup)
-    userSearch = input("What pet's profile do you want to see? \n1. Dutton \n2. Kiko \n 3. Copper? Press Enter.")
-
-    r = df["Weight"].loc[int(userSearch)]
-    print(r, "\n")
-
-#loads image
-def get_image(pup=puppy_profile_file):
-    df = pd.read_csv(pup)
-    r = 2
-    pupImage = df["Profile Image"].loc[r]
-    # load and show images
-    img = Image.open(pupImage)
-    display(img) # in jupyter, the image is shown as output
-
-#Calculates age     
-def getAge(pup=puppy_profile_file, csv_file=myfile): #DOB = '01/05/2023', , date_of_entry = '05/02/2023'
+#Calculates age
+def getAge(fileIndex, pup=puppy_profile_file): #DOB = '01/05/2023', , date_of_entry = '05/02/2023'
     """Calculate the age based on all of the Date fields from the PetRecord.csv file and compares to the DOB field from the pets.csv file."""
+    csv_file=petFile[fileIndex]
     df = pd.read_csv(pup)
     r = 1
     DOB = df["Date of Birth"].loc[r]
@@ -143,6 +137,27 @@ def getAge(pup=puppy_profile_file, csv_file=myfile): #DOB = '01/05/2023', , date
     print("Months: " + str(age/(60 * 60 * 24 * 30))) # (60 seconds * 60 minutes * 24 hours * 30 days)
     print("Years: " + str(age/(3600 * 24 * 365))) # ((60 seconds * 60 minutes) = 3600 seconds * 24 hours * 365 days)
 
+#loads image for profile
+def get_puppy_profile_image(pup=puppy_profile_file):
+    df = pd.read_csv(pup)
+    r = input("What image do you want?")
+    pupImage = df["Profile Image"].loc[int(r)]
+    # load and show images
+    img = Image.open(pupImage)
+    display(img) # in jupyter, the image is shown as output
+
+ #loads image
+def get_individual_image(fileIndex):
+
+    csv_file=petFile[fileIndex]
+    df = pd.read_csv(csv_file)
+    r = input("What image do you want?")
+    pupImage = df["Image"].loc[int(r)]
+    # load and show images
+    img = Image.open(pupImage)
+    display(img) # in jupyter, the image is shown as output   
+
+
 #Plots csv file into plotly
 def plotcsv():
     '''Plots pet csv file based on PetID selected by user. '''
@@ -151,22 +166,51 @@ def plotcsv():
 def display_UI():
     '''Print values per the unique PetIDs to generate buttons.'''
 
-#get_image()
-#add_new_record(myfile)
-#load_file()
-#getAge()
-#print_indvidual_entry_record
-#print_weight_record_for_individual()
-#Get_All_Profiles()
-#get_uniquedogprofile()
-#add_new_pet()
-
-
 # gets dog's profile based on PetID.
 def get_uniquedogprofile(pup=puppy_profile_file):
     '''returns dog's profiles based on petID.'''
     df = pd.read_csv(pup)
-    userSearch = input("What pet's profile do you want to see? \n1. Dutton \n2. Max  \n 3. Oliver? Press Enter.")
+    userSearch = input("What pet's profile do you want to see?")
 
-    r = df["Weight"].loc[int(userSearch)]
+    r = df["PetID"].loc[int(userSearch)]
     print(r, "\n")
+
+# Add to pet array and add it to the list
+def add_to_pet_array(file=petFile):
+    user_input = input("Enter a value to add to the array: ")
+    file.append(user_input)
+
+    # Print the updated array
+    print("Updated array:", file)
+
+# Delete to pet array
+def delete_to_pet_array(file=petFile):
+    user_input = input("Enter a value to delete from the array: ")
+    file.append(user_input)
+
+    print(df.drop(3, axis=0, inplace=False))
+    # Print the updated array
+    print("Updated array:", file)    
+
+delete_to_pet_array()
+#TheFile = whichfile()
+#load_file(TheFile)
+#add_to_pet_array()
+#add_new_pet()
+#print("My array " + str(petFile))
+#add_new_weight_record(TheFile)
+#add_new_pet()
+#Get_All_Profiles()
+#print_indvidual_entry_record(TheFile)
+#print_weight_record_for_individual(TheFile)
+#getAge(TheFile, puppy_profile_file)
+#get_puppy_profile_image()
+#get_individual_image(TheFile)
+#get_uniquedogprofile()
+
+
+
+#printFile = load_file(TheFile)
+#print(printFile)
+#get_uniquedogprofile()
+
